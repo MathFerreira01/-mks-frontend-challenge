5f482f481a3c49dd0940f44012920ccc2c483384
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-11-27 20:09:04 -0300

    criado o component Loading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
 import { CardList } from "../../components/CardList";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
-import { Loading } from '../../components/Loading';
 
 export function Home() {
-  const [removeLoading, setRemoveLoading] = useState(false);
 
   return (
     <>
       <Header />
-      {!removeLoading && <Loading />}
       <CardList/>
       <Footer/>
     </>
